d7acb8ab619a2c0032d506f3d4947cd6c1098574
Uncle Ben <jiasz527@qq.com>
2020-03-21 23:09:11 +0800

    Add index page.

--- watchlist-venv/app.py
+++ watchlist-venv/app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import escape
 from flask import url_for
 
 app = Flask(__name__)
+
+
+name = 'UncleBen'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 @app.route('/')
 def hello():
     #return "Welcome to My Watchlist!"
     #return u'欢迎来到我的 Watchlist！'
     #return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-    return 'Hello, there!'
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
