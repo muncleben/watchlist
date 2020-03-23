1bc99bdf45371a51979c7253a634325627852266
Uncle Ben <jiasz527@qq.com>
2020-03-23 22:56:53 +0800

    Add base template and error template.

--- watchlist-venv/app.py
+++ watchlist-venv/app.py
@@ -67,6 +67,16 @@ def index():
     movies = Movie.query.all()
     return render_template('index.html', user=user, movies=movies)
 
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s page' % escape(name)
